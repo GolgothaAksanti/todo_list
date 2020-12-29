9894b8890f6824a68d5e9892c26a0ed55a6c7437
GolgothaAksanti <38277247+GolgothaAksanti@users.noreply.github.com>
2020-12-29 03:37:08 +0300

    create a route that get all todos in the database

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
+const apiRoutes = require("./routes/apiRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(apiRoutes);
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
